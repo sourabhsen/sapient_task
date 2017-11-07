f33e19001258553c74c429ac3147740d53e461ce
sourabh sen <sourabhchill@gmail.com>
2017-11-07 10:36:34 +0530

    added

--- build/app/views/productlist/productlist-controller.js
+++ build/app/views/productlist/productlist-controller.js
@@ -3,13 +3,23 @@
 
   function ProductListController($scope, $http, ProductListService) {
     var vm = this;
-
+    vm.price = [];
     var getProductDetail = function getProductDetail() {
       ProductListService.getProductsInCarts().then(function (response) {
         vm.cart = response.data.productsInCart;
         vm.currency = response.data.productsInCart[0].c_currency;
+        angular.forEach(vm.cart, function (element, i) {
+          vm.price[i] = 1;
+          element.p_updatedprice = vm.price[i] * element.p_price;
+        });
       });
     };
+
+    vm.updatePrice = function (index, price) {
+      var price = Math.floor(price);
+      vm.cart[index].p_updatedprice = price * vm.cart[index].p_price;
+    };
+
     vm.onInit = function () {
       getProductDetail();
     };
